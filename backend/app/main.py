6d5c4388f36d1111600d7dc4ccabb413947233a8
GaoMeng <gaomeng@outlook.ie>
2023-04-11 17:44:44 +0800

    add citation with class

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from app.internal import auth, video
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import ahp, fce, dir, mark, set, websocket ,cnki
+from app.routers import ahp, fce, dir, mark, set, websocket ,cnki ,citation
 
 app = FastAPI()
 
@@ -26,6 +26,7 @@ app.include_router(set.router, prefix='/tools-next/api')
 app.include_router(websocket.router, prefix='/tools-next/api')
 app.include_router(video.router, prefix='/tools-next/api')
 app.include_router(cnki.router,prefix='/tools-next/api')
+app.include_router(citation.router,prefix='/tools-next/api')
 
 if __name__ == "__main__":
     # uvicorn.run(app="app.main:app", host="192.168.31.59", port=8000, reload=True, debug=True)
--- backend/app/routers/citation.py
+++ backend/app/routers/citation.py
@@ -1,90 +1,162 @@
 import re
-
 import win32com.client
+import json
+import os.path
+import uuid
+from lxml import etree
+import subprocess
+import win32com
+import win32com.client
+from fastapi import APIRouter, Depends, WebSocket, WebSocketDisconnect, UploadFile, status, HTTPException
+from ..internal.auth import check_token_exp_time
+from fastapi.responses import FileResponse
+from .websocket import manager
+
+router = APIRouter(dependencies=[Depends(check_token_exp_time)])
+
+positon_exception = HTTPException(
+    status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+    detail="并没有找到起点",
+)
+
+@router.websocket("/ws/citation-mark/upload_word/{timestamp}")
+async def upload_word_run(websocket: WebSocket, timestamp: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            params = await websocket.receive_text()
+            params = json.loads(params)  # receive params
+            # print(params)
+            file_bytes = await websocket.receive_bytes()  # receive word
+            prefix_name = str(timestamp) + params['filename']
+            f = open(os.path.abspath('.') + '\\temp\\word\\' + prefix_name, 'wb')
+            f.write(file_bytes)
+            f.close()
+            await manager.send_personal_json({'status': '上传已完成，准备标记'}, websocket)
+            citation_mark = CitationMark(os.path.abspath('.') + '\\temp\\word\\' + prefix_name)
+            citation_mark.get_citation_list()
+            await manager.send_personal_json({'status': '已获取到参考文献'},websocket)
+            citation_mark.make_footnotes()
+            await manager.send_personal_json({'status': '已完成脚注'}, websocket)
+            citation_mark.sort_to_positon()
+            await manager.send_personal_json({'status': '已完成整理工作，等待数据回传'}, websocket)
+            citation_mark.save_docx()
+            f = open(os.path.abspath('.') + '\\temp\\word\\' + prefix_name, 'rb')
+            file_bytes = f.read()
+            f.close()
+            await manager.send_personal_bytes(file_bytes, websocket)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
 
 
-path = ''
-# 打开Word程序并打开文档
-"""
-   https://learn.microsoft.com/en-us/office/vba/api/word.find.execute 
-"""
-word = win32com.client.DispatchEx("Word.Application")
-word.Visible = True
-word.DisplayAlerts = False
-docx = word.Documents.Open('C:\\Users\GM\Desktop\\1027文稿修改3_基于实物期权法的H光伏发电项目投资决策研）.docx')
-word.Selection.Find.IgnoreSpace = True
-word.Selection.Find.IgnorePunct = True
-word.Selection.Find.MatchByte = False
-word.Selection.Find.Forward = True
-word.Selection.Find.MatchWildcards = True
 
-# 使用Selection对象查找包含“参考文献”的段落
+class CitationMark:
 
-find_result = word.Selection.Find.Execute("<参考文献>")
-"""
-https://learn.microsoft.com/en-us/office/vba/api/word.wdunits
-"""
-paragraph_text_items = []
-if find_result:
-    word.Selection.MoveUntil(4)
+    def __init__(self,path):
+        self.path = path
+        self.citation_items = []
+        self.name_items = []
+        self.nofind = []
+        self.find_list = []
+        """
+           https://learn.microsoft.com/en-us/office/vba/api/word.find.execute 
+        """
+        self.word = win32com.client.DispatchEx("Word.Application")
+        self.word.Visible = True
+        self.word.DisplayAlerts = False
+        self.docx = self.word.Documents.Open(self.path)
+        self.word.Selection.Find.IgnoreSpace = True
+        self.word.Selection.Find.IgnorePunct = True
+        self.word.Selection.Find.MatchByte = False
+        self.word.Selection.Find.Forward = True
 
-    while 1 > 0:
-        word.Selection.MoveRight()
-        # 获取当前光标位置
-        selection = word.Selection
-        current_range = word.Selection.Range
+    def get_citation_list(self):
+        self.word.Selection.Find.MatchWildcards = True
+        find_result = self.word.Selection.Find.Execute("<参考文献>")
+        """
+        https://learn.microsoft.com/en-us/office/vba/api/word.wdunits
+        """
 
-        # 获取当前光标所在段落的范围
-        current_range.Paragraphs(1).Range.Select()
-        paragraph_range = selection.Range
+        if find_result:
+            self.word.Selection.MoveUntil(4)
+            while 1 > 0:
+                self.word.Selection.MoveRight()
+                # 获取当前光标位置
+                selection = self.word.Selection
+                current_range = self.word.Selection.Range
 
-        # 获取段落内容
-        paragraph_text = paragraph_range.Text
-        word.Selection.Delete()
-        if len(paragraph_text) > 12:
-            paragraph_text_items.append(paragraph_text)
+                # 获取当前光标所在段落的范围
+                current_range.Paragraphs(1).Range.Select()
+                paragraph_range = selection.Range
+
+                # 获取段落内容
+                paragraph_text = paragraph_range.Text
+                self.word.Selection.Delete()
+                if len(paragraph_text) > 12:
+                    self.citation_items.append(paragraph_text)
+                else:
+                    break
+
         else:
-            break
+            raise positon_exception
 
-else:
-    print("并没有找到起点")
+    def make_footnotes(self):
+        self.name_items = list(map(lambda x: re.sub(r'(?=\.).*', '', x), self.citation_items))
+        self.name_items = list(map(lambda x: re.sub(r',.*', '', x), self.name_items))
 
-word.Selection.Find.MatchWildcards = False
-name_items = list(map(lambda x: re.sub(r'(?=\.).*', '', x), paragraph_text_items))
-name_items = list(map(lambda x: re.sub(r',.*', '', x), name_items))
-nofind =[]
-for index, item in enumerate(name_items):
-    word.Selection.WholeStory()
-    result = word.Selection.Find.Execute(item)
-    if result:
-        word.Selection.MoveRight()
-        word.Selection.TypeText("[clone]")
-        docx.Footnotes.Add(word.Selection.Range)
-        word.Selection.TypeText(paragraph_text_items[index].replace("\r", ""))
-        word.Selection.WholeStory()
-        if index == len(name_items) -1 :
-            break
-        word.Selection.MoveUp(5)
-    else:
-        nofind.append(item)
+        self.word.Selection.Find.MatchWildcards = False
+        for index, item in enumerate(self.name_items):
+            self.word.Selection.WholeStory()
+            result = self.word.Selection.Find.Execute(item)
+            if result:
+                self.word.Selection.MoveRight()
+                self.word.Selection.Find.Execute("。")
+                self.word.Selection.MoveLeft()
+                self.word.Selection.TypeText("[clone]")
+                self.docx.Footnotes.Add(self.word.Selection.Range)
+                self.word.Selection.TypeText(self.citation_items[index].replace("\r", ""))
+                self.find_list.append(self.citation_items[index])
+                self.word.Selection.WholeStory()
+                if index == len(self.name_items) - 1:
+                    break
+                self.word.Selection.MoveUp(5)
+            else:
+                self.nofind.append(item)
+    def sort_to_positon(self):
+        self.word.Selection.MoveUp(5)
+        self.word.Selection.WholeStory()
+        self.word.Selection.Find.MatchWildcards = True
+        result = self.word.Selection.Find.Execute("<参考文献>")
+        if result:
+            self.word.Selection.MoveRight()
+            self.word.Selection.TypeParagraph()
+            for _ in self.find_list:
+                self.word.Selection.TypeText(_)
+                self.word.Selection.MoveRight()
+            self.word.Selection.TypeParagraph()
+            self.word.Selection.TypeParagraph()
 
-find_itmes = []
+            for _ in self.nofind:
+                self.word.Selection.Font.ColorIndex = 7
+                self.word.Selection.TypeText(_)
+                self.word.Selection.MoveRight()
 
-footnotes_range = word.Selection.Range
-for paragraph in footnotes_range.Paragraphs:
-    find_itmes.append(paragraph.Range.Text.replace(" ",""))
+        self.word.Selection.Find.MatchWildcards = False
+        self.word.Selection.WholeStory()
+        self.word.Selection.Find.Forward = True
+        self.word.Selection.Find.Wrap = 0
+        self.word.Selection.Find.Replacement.Font.Superscript = True
+        count = 0
+        while True:
+            count += 1
+            # self.word.Selection.Find.Text = "[clone]^f"
+            # self.word.Selection.Find.Replacement.Text = "[" + str(count) + "]"
+            if not self.word.Selection.Find.Execute("[clone]^f", False, False, False, False, False, True, 1, True, "[" + str(count) + "]", 1):
+                break  # 没有符合条件的查找结果，结束循环
+            # self.word.Selection.Range.Text = "[" + str(count) + "]"
+            self.word.Selection.Start = self.word.Selection.End
+    def save_docx(self):
+        self.docx.Save()
+        self.docx.Close()
+        self.word.Quit()
 
-word.Selection.MoveUp(5)
-word.Selection.WholeStory()
-word.Selection.Find.MatchWildcards = True
-find_result = word.Selection.Find.Execute("<参考文献>")
-word.Selection.MoveRight()
-word.Selection.TypeParagraph()
-for _ in find_itmes:
-    word.Selection.TypeText(_)
-    word.Selection.MoveRight()
-word.Selection.TypeText("____未进行标记____")
-word.Selection.TypeParagraph()
-for _ in nofind:
-    word.Selection.TypeText(_.replace("\r", ""))
-    word.Selection.MoveRight()
